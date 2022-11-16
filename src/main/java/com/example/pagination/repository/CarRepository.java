43277837f09d155fffa2416381bc23b01f78f3d4
gulernurdonmez <gule0153@stud.kea.dk>
2022-11-16 10:57:38 +0100

    added methods

--- src/main/java/com/example/pagination/repository/CarRepository.java
+++ src/main/java/com/example/pagination/repository/CarRepository.java
@@ -2,6 +2,14 @@ package com.example.pagination.repository;
 
 import com.example.pagination.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car,Integer> {
+
+    List<Car> findCarByBrand(String brand);
+    List<Car> findCarByColor(String color);
 }
+
+/*public interface CarRepository extends PagingAndSortingRepository{}*/
